740e5dec6adc7a1be07ebc1a30c83a4994771c5b
MoNTE48 <MoNTE48@mail.ua>
2019-10-24 20:59:32 +0200

    Android ver, temp solution

--- src/config.h
+++ src/config.h
@@ -15,7 +15,10 @@
 	#define PROJECT_NAME "minetest"
 	#define PROJECT_NAME_C "Minetest"
 	#define STATIC_SHAREDIR ""
-	#include "android_version.h"
+	#define VERSION_MAJOR 5
+	#define VERSION_MINOR 1
+	#define VERSION_PATCH 0
+	#define VERSION_STRING STR(VERSION_MAJOR) "." STR(VERSION_MINOR) "." STR(VERSION_PATCH)
 	#ifdef NDEBUG
 		#define BUILD_TYPE "Release"
 	#else
